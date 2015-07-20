5937715ed2d4f4df6ae15e60160a5061cca711b8
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2015-07-20 13:11:03 -0400

    Block sampling works!!!! No rsd sampling yet

--- test-setup.R
+++ test-setup.R
@@ -1,12 +1,66 @@
 # Test block sampling on a simple cubic model
-model <- function(b,x) b[1]*x^3 + b[2]*x^2 + b[3]*x + b[4]
-coefs <- c(4,3,2,1)
-x <- seq(1, 100, by=0.1)
-obs <- model(coefs, x) + rnorm(length(x), 0, 200*x)
-plot(x,obs, pch=".")
+library(minpack.lm)
+library(PEcAnRTM)
+library(MASS)
+true.vals <- c("N"=1.7, "Cab"=32, "Cw"=0.01, "Cm"=0.008)
+obs <- prospect(true.vals, 4)
 
-loglike <- function(b){
-    error <- model(b,x) - obs
-    logl <- sum(dnorm(error, 0, 0.5))
-    return(logl)
+ngibbs <- 10000
+adapt <- 100
+accept <- 0
+target <- 0.44
+adjmin <- 0.1
+pmin <- c(1,0,0,0)
+#inits <- true.vals
+inits <- c(2, 40, 0.001, 0.001)
+initsd <- inits*0.01
+Jump <- diag(initsd)
+
+result <- matrix(NA, nrow=ngibbs, ncol=4)
+for(n in 1:ngibbs){
+    if(n > adapt && n %% adapt == 1){
+        if(accept == 0){
+            rescale <- diag(rep(adjmin, 4))
+            Jump <- rescale %*% Jump %*% rescale
+        }
+        else {
+            arate <- accept/adapt
+            region <- (n-adapt):(n-1)
+            stdev <- apply(result[region,], 2, sd)
+            stdev[is.na(stdev)] <- initsd[is.na(stdev)]
+            adjust <- max(arate/target, adjmin)
+            rescale <- diag(stdev * adjust)
+            corr <- cor(result[region,])
+            if(any(is.na(corr))) corr <- diag(rep(1,4))
+            Jump <- rescale %*% corr %*% rescale
+        }
+        print(diag(Jump))
+        accept <- 0
+    }
+    guess <- mvrnorm(1, inits, Jump)
+    if(any(guess < pmin)){
+        result[n,] <- inits
+        next
+    }
+    guess.obs <- prospect(guess, 4)
+    guess.error <- guess.obs - obs
+    guess.like <- sum(dnorm(guess.error, 0, 0.1, log=TRUE))
+    prev.obs <- prospect(inits, 4)
+    prev.error <- prev.obs - obs
+    prev.like <- sum(dnorm(prev.error, 0, 0.1, log=TRUE))
+    a <- exp(guess.like - prev.like)
+    if(a > runif(1)){
+        accept <- accept + 1
+        inits <- guess
+    }
+    result[n,] <- inits
 }
+
+par(mfrow=c(4,2))
+for(i in 1:4){
+    plot(result[,i], type='l')
+    abline(h = true.vals[i], col="red")
+    plot(density(result[-5000:0,i]))
+    abline(v = true.vals[i], col="red")
+}
+
